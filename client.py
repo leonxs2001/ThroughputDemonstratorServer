4bfba847c4971eb3f08f50c439378d6a133dc882
fbi-user <schoenbe@th-brandenburg.de>
2024-05-28 13:48:07 +0200

    Adds file download

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ from enums import DataUnit, SendingType
 
 HOST = '127.0.0.1'
 PORT = 65432
+BUFFER_SIZE = 1024
 
 
 def get_buffer_size():
@@ -42,38 +43,65 @@ def start_client():
         s.connect((HOST, PORT))
 
         s.sendall(sending_type.value.encode())
+        ready_signal = s.recv(1)
 
-        if sending_type == SendingType.DUMMY:
-            ready_signal = s.recv(1)
+        if ready_signal == b'\x01':
+            execution_time_seconds = 0
+            if sending_type == SendingType.DUMMY:
+                    s.sendall(f"{buffer_size};{data_size}".encode())
 
-            if ready_signal == b'\x01':
-                s.sendall(f"{buffer_size};{data_size}".encode())
+                    print(f"Received Data 0%")
+                    start_time = time.time()
 
-                print(f"Received Data 0%")
-                start_time = time.time()
+                    numbers_of_iteration = data_size / buffer_size
+                    for i in range(int(numbers_of_iteration)):
+                        s.recv(buffer_size)
+                        percent = ((i + 1) / numbers_of_iteration) * 100
+                        print(f"Received Data {percent:.2f}%")
 
-                numbers_of_iteration = data_size / buffer_size
-                for i in range(int(numbers_of_iteration)):
-                    s.recv(buffer_size)
-                    percent = ((i + 1) / numbers_of_iteration) * 100
-                    print(f"Received Data {percent:.2f}%")
+                    difference = numbers_of_iteration - int(numbers_of_iteration)
+                    if difference:
+                        s.recv(int(difference * buffer_size))
+                        print(f"Received Data 100%")
+                    end_time = time.time()
 
-                difference = numbers_of_iteration - int(numbers_of_iteration)
-                if difference:
-                    s.recv(int(difference * buffer_size))
-                    print(f"Received Data 100%")
-                end_time = time.time()
-
-                execution_time_seconds = end_time - start_time
-
-                execution_time_minutes = execution_time_seconds // 60
-                execution_time_seconds -= execution_time_minutes * 60
-                execution_time_milliseconds = int((execution_time_seconds - int(execution_time_seconds)) * 1000)
-                execution_time_seconds = int(execution_time_seconds)
-                print(
-                    f"Receiving ended. Execution time is {execution_time_minutes} minutes, {execution_time_seconds} seconds and {execution_time_milliseconds} milliseconds.")
+                    execution_time_seconds = end_time - start_time
             else:
-                print("Can not start sending data.")
+                s.sendall(str(buffer_size).encode())
+                filename, filesize_data = s.recv(BUFFER_SIZE).decode().split(";")
+                filesize = int(filesize_data)
+                s.sendall(b'\x01')
+
+                numbers_of_iteration = filesize / buffer_size
+                with open(f"{filename}", 'wb') as file:
+                    start_time = time.time()
+                    for i in range(int(numbers_of_iteration)):
+                        chunk = s.recv(buffer_size)
+                        if not chunk:
+                            break
+                        file.write(chunk)
+                        percent = ((i + 1) / numbers_of_iteration) * 100
+                        print(f"Received Data {percent:.2f}%")
+
+                    difference = numbers_of_iteration - int(numbers_of_iteration)
+                    if difference:
+                        chunk = s.recv(buffer_size)
+                        if chunk:
+                            file.write(chunk)
+                        print(f"Received Data 100%")
+                    end_time = time.time()
+                    execution_time_seconds = end_time - start_time
+
+            execution_time_minutes = execution_time_seconds // 60
+            execution_time_seconds -= execution_time_minutes * 60
+            execution_time_milliseconds = int((execution_time_seconds - int(execution_time_seconds)) * 1000)
+            execution_time_seconds = int(execution_time_seconds)
+            print(
+                f"Receiving ended. Execution time is {execution_time_minutes} minutes, {execution_time_seconds} seconds and {execution_time_milliseconds} milliseconds.")
+
+        else:
+            print("Can not start sending data. Server is not ready")
+
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,38 +1,62 @@
+import os
 import socket
 from enums import DataUnit, SendingType
 
-HOST = '127.0.0.1'
+HOST = '127.0.0.1' #"192.168.137.7"  # '127.0.0.1'
 PORT = 65432
 
 BUFFER_SIZE = 1024
 DATA_AMOUNT = 1
 DATA_UNIT = DataUnit.GB
 DATA_SIZE = DATA_AMOUNT * DATA_UNIT.value
+DOWNLOADABLE_FILE = r"C:\Users\fbi-user\Documents\5G\test.txt"
 
 
 def start_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
-        s.listen()
-        print('Server is waiting for connection...')
-        conn, addr = s.accept()
-        with conn:
-            print('Connected with', addr)
+        while True:
+            try:
+                s.listen()
+                print('Server is waiting for connection...')
+                conn, addr = s.accept()
+                with conn:
+                    print('Connected with', addr)
 
-            sending_type = SendingType.from_string(conn.recv(1024).decode())
-            if sending_type == SendingType.DUMMY:
-                conn.sendall(b"\x01")
-                buffer_size_data, data_size_data = conn.recv(1024).decode().split(";")
-                print((buffer_size_data, data_size_data))
-                buffer_size = int(buffer_size_data)
-                data_size = int(data_size_data)
-                numbers_of_iteration = data_size / buffer_size
-                for _ in range(int(numbers_of_iteration)):
-                    conn.sendall(b"A" * buffer_size)
+                    sending_type = SendingType.from_string(conn.recv(BUFFER_SIZE).decode())
+                    conn.sendall(b"\x01")
 
-                missing_data = b"A" * (data_size - buffer_size * int(numbers_of_iteration))
-                if missing_data:
-                    conn.sendall(missing_data)
+                    if sending_type == SendingType.DUMMY:
+                        buffer_size_data, data_size_data = conn.recv(BUFFER_SIZE).decode().split(";")
+
+                        buffer_size = int(buffer_size_data)
+                        data_size = int(data_size_data)
+                        numbers_of_iteration = data_size / buffer_size
+                        for _ in range(int(numbers_of_iteration)):
+                            conn.sendall(b"A" * buffer_size)
+
+                        missing_data = b"A" * (data_size - buffer_size * int(numbers_of_iteration))
+                        if missing_data:
+                            conn.sendall(missing_data)
+                    else:
+                        buffer_size = int(conn.recv(BUFFER_SIZE).decode())
+
+                        filename = os.path.basename(DOWNLOADABLE_FILE)
+                        filesize = os.path.getsize(DOWNLOADABLE_FILE)
+
+                        conn.sendall(f"{filename};{filesize}".encode())
+
+                        if conn.recv(1) == b"\x01":
+                            with open(DOWNLOADABLE_FILE, 'rb') as f:
+                                while True:
+                                    bytes_read = f.read(buffer_size)
+                                    if not bytes_read:
+                                        break
+                                    conn.sendall(bytes_read)
+                        else:
+                            print("Can not start sending data. Client is not ready.")
+            except:
+                continue
 
 
 if __name__ == '__main__':
